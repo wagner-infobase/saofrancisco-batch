9a7927b02fdb3dedc810e6fe852d261489f0a13a
wagner-infobase <wagner.lima@infobase.com.br>
2017-04-14 11:12:47 -0300

    Implementando upload de arquivos

--- batch-service/src/main/java/br/com/druid/batch/BatchServiceApplication.java
+++ batch-service/src/main/java/br/com/druid/batch/BatchServiceApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BatchServiceApplication {
+	
+	private int maxUploadSizeInMb = 10 * 1024 * 1024; // 10 MB
 
 	public static void main(String[] args) {
 		SpringApplication.run(BatchServiceApplication.class, args);
